381ef0a3081f7b01a695f11fdd13454b469aacd8
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-24 14:55:03 +0100

    add first test content

--- HotelLinenManagerV2.Tests/NullOrEmptyCheckerTests.cs
+++ HotelLinenManagerV2.Tests/NullOrEmptyCheckerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,45 @@ using System.Threading.Tasks;
 
 namespace HotelLinenManagerV2.Tests
 {
+
+    //parametr 1 jest null lub empty =>(zwraca) true
+    //parametr 2 jest null lub empty => true
+    //parametr 3 jest null lub empty => true
+    //Jezeli któryś z nich nie jest nullem, ma jakąś zawarotść wtedy metoda zwraca false
+    //Czy istnieje jakiś parametr, kótry może spowoidować błąd? np nie da się go zrobić to String?
+    //Tak bład jest taki że że parameter nie może być nulllem bo wtedy nie może wywnioskować typu
+
+    // Generalnie metoda do przeróbki, ponieważ po co ma ona generyczne parametry jezeli jako argumenty w handlerze
+    //podaje stringi? dlaczego wśród nich nie podaje NIP-u? -bo wtedy zawsze by odpytał APIGUS a nie chce tego, który de facto też jest stringiem.
+
+    // Sprawdzam czy miasto, ulica i nazwa firmy jest nulem lub jest pusta - zawsze przyjdzie puste(chyba ze wypełnione bo tak 
+    //zrobiłem clienta ale nie powinno być. A sprawdzam te trzy po prostu dlaetego, że wiem ze jak są puste to mam odpytac gusowskie API
+    // jak sa wypełnione to znaczy ze formularz został wypełniony danymi
+
+
     [TestClass]
-   public class NullOrEmptyCheckerTests
+    public class NullOrEmptyCheckerTests
     {
+        [TestMethod]
+        public void MinOneParamIsTrue()
+        {
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void EveryParamIsFalse()
+        {
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DontHaveIdeaYet()
+        {
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void DontHaveIdeaYet_UsingTryCatch()
+        {
+            Assert.Inconclusive();
+        }
     }
 }
