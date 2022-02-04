bfd2f78649663d043778ce1b60a6bcf9ec0b0e5b
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-02-04 11:43:10 +0100

    Add object test examples

---
+++ HotelLinenManagerV2.Tests/Examples/ObjectTestExamples.cs
@@ -0,0 +1,48 @@
+﻿using HotelLinenManagerV2.Tests.PasswordHasherTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLinenManagerV2.Tests.Examples
+{
+    [TestClass]
+    public class ObjectTestExamples : TestBase
+    {
+        [TestMethod]
+        [Description("Checking: is object user not null")]
+        public void IsNullTest()
+        {
+            //Arrange
+            var user = new User();
+            //Act
+            //Assert
+            Assert.IsNotNull(user);
+           
+        }
+        [TestMethod]
+        [Description("Checking: is user a User instance")]
+        public void IsInstanceOfTypeTest()
+        {
+            //Arrange
+            var user = new User();
+            //Assert
+            Assert.IsInstanceOfType(user, typeof(User));    
+        }
+        [TestMethod]
+        [Description("Checking: are object the same?")]
+        public void AreTheSame()
+        {
+            //Arrange
+            var x = new User();
+            var y = new User(); 
+            //Assert
+            Assert.AreNotSame(x, y);
+
+
+        }
+
+    }
+}
